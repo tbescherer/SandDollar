47ae9d4d44bf4161410123aaf24dc9be5ec03287
Tom Bescherer <Tbescherer@gmail.com>
2015-06-21 18:27:05 -0400

    Finish views for user profile

--- app/controllers/house_rental_requests_controller.rb
+++ app/controllers/house_rental_requests_controller.rb
@@ -5,7 +5,9 @@ class HouseRentalRequestsController < ApplicationController
   def create
     @request = HouseRentalRequest.new(rental_params)
     @request.user_id = current_user.id
+    @request.status = "Pending"
     if @request.save
+      flash[:errors] = ["House request Submitted!"]
       redirect_to house_url(@request.house)
     else
       flash[:errors] = @request.errors.full_messages
@@ -18,9 +20,18 @@ class HouseRentalRequestsController < ApplicationController
   end
 
   def edit
+    @request = HouseRentalRequest.find(params[:id])
   end
 
   def update
+    @request = HouseRentalRequest.find(params[:id])
+    if @request.update(rental_params)
+      flash[:errors] = ["submission accepted"]
+      redirect_to user_url(current_user)
+    else
+      flash[:errors] = ["something went awry!"]
+      render :edit
+    end
   end
 
   private
