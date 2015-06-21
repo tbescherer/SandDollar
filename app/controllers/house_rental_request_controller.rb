1faa17efb8d4aa071e612e12ed03a6085a0fd4ed
Tom Bescherer <Tbescherer@gmail.com>
2015-06-21 17:25:52 -0400

    Refactor

---
+++ app/controllers/house_rental_request_controller.rb
@@ -0,0 +1,17 @@
+class HouseRentalRequestController < ApplicationController
+
+  before_action :require_signed_in
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
