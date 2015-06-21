d477628fafe2c146e89ab7790c058a99ae27b602
Tom Bescherer <Tbescherer@gmail.com>
2015-06-21 16:08:16 -0400

    Create houses controller

---
+++ app/controllers/houses_controller.rb
@@ -0,0 +1,48 @@
+class HousesController < ApplicationController
+
+  def new
+    @house = current_user.houses.new
+  end
+
+  def create
+    @house = current_user.houses.new(house_params)
+    if @house.save
+      redirect_to house_url(@house)
+    else
+      flash[:errors] = @house.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    @house = House.find(params[:id])
+    render :show
+  end
+
+  def index
+    render :index
+  end
+
+  def edit
+    @house = House.find(params[:id])
+    if @house.update(house_params)
+      redirect_to house_url(@house)
+    else
+      flash[:errors] = @house.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+    flash[:errors] = ["House successfully deleted"]
+    redirect_to houses_url
+  end
+
+  private
+
+  def house_params
+    params.require(:house).permit(:owner_id, :decription, :address)
+  end
+end
